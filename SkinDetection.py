0e5199bb91357b9140cc9d0a40559be0ceddf84a
Mayank Kumar Jha <mk9440@users.noreply.github.com>
2017-03-20 01:01:39 +0530

    added Skin Visibility

--- SkinDetection.py
+++ SkinDetection.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 19 14:48:31 2017
+Created on Mon Mar 20 00:43:20 2017
 
 @author: mayank
 """
@@ -8,17 +8,30 @@ Created on Sun Mar 19 14:48:31 2017
 import cv2
 import numpy as np
 
-cap=cv2.VideoCapture(0)
-lower = np.array([0, 10, 60], dtype = "uint8")
-upper = np.array([20, 150, 255], dtype = "uint8")
-while(True):
-    ret,frame=cap.read()
-    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    mask=cv2.inRange(frame,lower,upper)
-    cv2.resizeWindow('cam',640,320)
-    cv2.imshow('cam',mask)
-    if cv2.waitKey(1)==ord('q') :
-        break;
-        
-cap.release()        
-cv2.destroyAllWindows()    
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of skin color in HSV
+    lower = np.array([0, 10, 60], dtype = "uint8") 
+    upper = np.array([20, 150, 255], dtype = "uint8")
+    # Threshold the HSV image to get only skin colors
+    mask = cv2.inRange(hsv, lower, upper)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    #cv2.imshow('frame',frame)
+    #cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    if cv2.waitKey(1)==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
