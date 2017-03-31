d22aadcbaec29f23fa3f765824a6dd2e0f7b7ca3
Mayank Kumar Jha <mk9440@users.noreply.github.com>
2017-03-31 21:49:13 +0530

    Skin-Detection-with video recording and player

--- SkinDetection.py
+++ SkinDetection.py
@@ -1,37 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 20 00:43:20 2017
+Created on Sat Mar 18 20:01:14 2017
 
 @author: mayank
 """
 
+
+import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+lower = np.array([0, 10, 60], dtype = "uint8")
+upper = np.array([20, 150, 255], dtype = "uint8")
 
-while(1):
 
-    # Take each frame
-    _, frame = cap.read()
-
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # define range of skin color in HSV
-    lower = np.array([0, 10, 60], dtype = "uint8") 
-    upper = np.array([20, 150, 255], dtype = "uint8")
-    # Threshold the HSV image to get only skin colors
-    mask = cv2.inRange(hsv, lower, upper)
-
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-
-    #cv2.imshow('frame',frame)
-    #cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    if cv2.waitKey(1)==ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+try:
+    
+    cap=cv2.VideoCapture(0)
+    fourcc=cv2.VideoWriter_fourcc(*'DIVX')
+    output=cv2.VideoWriter('D:/Development/Python Dev/capture1.mp4',fourcc,1.0,(640,480),True)
+       
+    while(True):
+        ret,frame=cap.read()
+        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        mask=cv2.inRange(hsv,lower,upper)
+        hsv=cv2.bitwise_and(frame,frame,mask=mask)
+        output.write(hsv)
+        cv2.resizeWindow('Cam',640,460)
+        cv2.imshow('Cam',frame)
+        if cv2.waitKey(1)==ord('q'):
+            break
+       
+    cap.release()
+    output.release()
+    cv2.destroyAllWindows()
+    cap=cv2.VideoCapture('D:/Development/Python Dev/capture1.mp4')
+    while(cap.isOpened()):
+        ret,f=cap.read()
+        cv2.resizeWindow('Cam',640,460)
+        cv2.imshow('Cam',f)
+        if cv2.waitKey(1)==ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()        
+except:
+    cap.release()
+    output.release()
+    cv2.destroyAllWindows() 
+       
